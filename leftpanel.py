6985aec9d68f90d54cc4b5ee054deb9241474923
JipsonFrancis <francis.boy.jipson.jnr@gmail.com>
2025-04-18 14:39:03 +0200

    Population integrated
    No Flickering, initial integration caused a flicker, upon close inspection populationController had a pygmae update function which was supposed to be in the main.py only

--- leftpanel.py
+++ leftpanel.py
@@ -1,13 +1,12 @@
 import pygame
-from sysui import SysUI
+from sysui import PanelUI
 
-class LeftPanel:
+class LeftPanel(PanelUI):
     def __init__(self):
-        self.sysui = SysUI()
+        super().__init__()
 
     def draw(self):
-        self.left_rect = pygame.Rect(self.sysui.panel_padding, self.sysui.panel_top, self.sysui.panel_width, self.sysui.panel_height)
-        pygame.draw.rect(self.sysui.ui.screen, (190, 190, 190), self.left_rect, 3)
-        self.pop_label = self.sysui.ui.font.render("Population", True, (0, 0, 0))
-        self.sysui.ui.screen.blit(self.pop_label, self.pop_label.get_rect(center=(self.left_rect.centerx, self.left_rect.top - 20)))
-        # pygame.display.update()
+        self.left_rect = pygame.Rect(self.panel_padding, self.panel_top, self.panel_width, self.panel_height)
+        pygame.draw.rect(self.screen, (190, 190, 190), self.left_rect, 3)
+        self.pop_label = self.font.render("Population", True, (0, 0, 0))
+        self.screen.blit(self.pop_label, self.pop_label.get_rect(center=(self.left_rect.centerx, self.left_rect.top - 20)))
--- sysui.py
+++ sysui.py
@@ -1,13 +1,25 @@
 from ui import UI
 
-class SysUI:
+class PanelUI(UI):
     def __init__(self):
-        self.ui = UI()
-            
+        super().__init__()
         # Layout constants
         self.panel_padding = 3
-        self.panel_width = self.ui.width * 0.25 - 2 * self.panel_padding
-        self.panel_height = self.ui.height * 0.7
+        self.panel_width = self.width * 0.25 - 2 * self.panel_padding
+        self.panel_height = self.height * 0.7
         self.panel_top = 130
-        self.center_x = self.ui.width // 2
-        self.center_y = self.ui.height // 2 + 20
+        self.center_x = self.width // 2
+        self.center_y = self.height // 2 + 20
+    
+    def get_elements(self):
+        """
+        Should return elements to be blitted or drawn.
+        Must be implemented by subclasses.
+        """
+        # elements = []
+        # if last_spoke_hit and angular_velocity == 0:
+        #     label, pos = self.create_text_label(f"Last Hit: {last_spoke_hit.data}", (10, 10))
+        #     elements.append((label, pos))
+        # return elements
+    
+        pass
