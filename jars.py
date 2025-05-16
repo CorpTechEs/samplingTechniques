3342515facb25ccc8a2b1eddb01fcd3d1647f0ee
JipsonFrancis <francis.boy.jipson.jnr@gmail.com>
2025-05-16 23:29:36 +0200

    jar n the ui
    all ui essential ui elements are on screen

--- jars.py
+++ jars.py
@@ -11,7 +11,17 @@ class CollectionJar (PanelUI):
         self.u_area_start_x = (self.width // 2) - (self.total_u_width // 2)
         self.u_y_top = self.panel_top + self.panel_height - self.u_box_size  # Align with bottom of panels
 
+        self.jarA = self.load_image("./uiElement/JARa.png")
+        self.jarB = self.load_image("./uiElement/JARb.png")
+        self.jarC = self.load_image("./uiElement/JARc.png")
+        self.jarD = self.load_image("./uiElement/JARd.png")
+
     def draw(self):
+        self.screen.blit(self.jarA, (800, 575))  # draw jar
+        self.screen.blit(self.jarB, (800, 395))  # draw jar
+        self.screen.blit(self.jarC, (800, 200))  # draw jar
+        self.screen.blit(self.jarD, (800, 0))    # draw jar
+
         # === U-shaped Squares at Bottom, spread in center 50% ===
         for i in range(self.jars):
             x = self.u_area_start_x + i * (self.u_box_size + self.u_gap)
